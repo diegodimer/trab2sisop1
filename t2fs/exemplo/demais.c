44e190796ced901008252aa8ffbe5a571cee0318
Diego Dimer <diego.dimer@ufrgs.br>
2019-06-05 19:48:11 -0300

    Makefiles organizados + main do prof

---
+++ t2fs/exemplo/demais.c
@@ -0,0 +1,12 @@
+#include <t2fs.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    format2(10);
+}
+
+
+
--- t2fs/src/t2fs.c
+++ t2fs/src/t2fs.c
@@ -1,6 +1,4 @@
 
-/**
-*/
 #include "t2fs.h"
 
 /*-----------------------------------------------------------------------------
@@ -12,18 +10,21 @@ int identify2 (char *name, int size) {
 
 /*-----------------------------------------------------------------------------
 Função:	Formata logicamente o disco virtual t2fs_disk.dat para o sistema de
-		arquivos T2FS definido usando blocos de dados de tamanho 
+		arquivos T2FS definido usando blocos de dados de tamanho
 		corresponde a um múltiplo de setores dados por sectors_per_block.
 -----------------------------------------------------------------------------*/
 int format2 (int sectors_per_block) {
-	return -1;
+	if(sectors_per_block < 2 || sectors_per_block % 1024 != 0 || sectors_per_block >= 1024){
+        return -1;
+	}
+	return 0;
 }
 
 /*-----------------------------------------------------------------------------
 Função:	Função usada para criar um novo arquivo no disco e abrí-lo,
 		sendo, nesse último aspecto, equivalente a função open2.
-		No entanto, diferentemente da open2, se filename referenciar um 
-		arquivo já existente, o mesmo terá seu conteúdo removido e 
+		No entanto, diferentemente da open2, se filename referenciar um
+		arquivo já existente, o mesmo terá seu conteúdo removido e
 		assumirá um tamanho de zero bytes.
 -----------------------------------------------------------------------------*/
 FILE2 create2 (char *filename) {
@@ -31,7 +32,7 @@ FILE2 create2 (char *filename) {
 }
 
 /*-----------------------------------------------------------------------------
-Função:	Função usada para remover (apagar) um arquivo do disco. 
+Função:	Função usada para remover (apagar) um arquivo do disco.
 -----------------------------------------------------------------------------*/
 int delete2 (char *filename) {
 	return -1;
@@ -68,7 +69,7 @@ int write2 (FILE2 handle, char *buffer, int size) {
 }
 
 /*-----------------------------------------------------------------------------
-Função:	Função usada para truncar um arquivo. Remove do arquivo 
+Função:	Função usada para truncar um arquivo. Remove do arquivo
 		todos os bytes a partir da posição atual do contador de posição
 		(current pointer), inclusive, até o seu final.
 -----------------------------------------------------------------------------*/
@@ -134,7 +135,7 @@ int closedir2 (DIR2 handle) {
 
 /*-----------------------------------------------------------------------------
 Função:	Função usada para criar um caminho alternativo (softlink) com
-		o nome dado por linkname (relativo ou absoluto) para um 
+		o nome dado por linkname (relativo ou absoluto) para um
 		arquivo ou diretório fornecido por filename.
 -----------------------------------------------------------------------------*/
 int ln2 (char *linkname, char *filename) {
